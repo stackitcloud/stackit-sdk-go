e5b4474d41b4cdbf09f4e3bd6e21e8d016af2f58
João Palet <joao.palet@outlook.com>
2023-09-18 09:31:24 +0100

    Adjust condition

--- scripts/sync-tidy.sh
+++ scripts/sync-tidy.sh
@@ -31,8 +31,9 @@ done
 
 # Fail when changes are produced
 git add -A
-echo "All go.mod files are synced and tidy."
-if ! git diff --cached --exit-code --quiet; then # --cached flag needed so new files are considered
-    echo "Please run "make sync-tidy" locally to sync dependencies and tidy go.mod files. Commit the changes."
+if git diff --cached --exit-code --quiet; then # --cached flag needed so new files are considered
+    echo "All go.mod files are is synced and tidy."
+else
+    echo "Please run "make sync-tidy" locally to sync dependencies and tidy go.mod files."
     exit 1
 fi
