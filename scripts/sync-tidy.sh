4d07910772f468f058b86370bb945a7efcb4221e
João Palet <joao.palet@outlook.com>
2023-09-18 11:39:33 +0200

    Fix typo
    Co-authored-by: Vicente Pinto <vicente.pinto@freiheit.com>

--- scripts/sync-tidy.sh
+++ scripts/sync-tidy.sh
@@ -32,7 +32,7 @@ done
 # Fail when changes are produced
 git add -A
 if git diff --cached --exit-code --quiet; then # --cached flag needed so new files are considered
-    echo "All go.mod files are is synced and tidy."
+    echo "All go.mod files are synced and tidy."
 else
     echo "Please run "make sync-tidy" locally to sync dependencies and tidy go.mod files. Commit the changes."
     exit 1
