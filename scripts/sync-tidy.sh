b4e8d03b276f87e3864b0c2c341ee685dfce0e44
João Palet <joao.palet@outlook.com>
2023-09-18 12:11:43 +0200

    Enforce go.mod files are synced and tidy in the CI (#14)
    * Enforce go.mod files are synced and tidy in the CI
    
    * Fix typo
    
    Co-authored-by: Vicente Pinto <vicente.pinto@freiheit.com>
    
    * Adjust message
    
    Co-authored-by: Vicente Pinto <vicente.pinto@freiheit.com>
    
    * Adjust condition
    
    * Adjust message
    
    * Fix typo
    
    Co-authored-by: Vicente Pinto <vicente.pinto@freiheit.com>
    
    ---------
    
    Co-authored-by: Vicente Pinto <vicente.pinto@freiheit.com>

--- scripts/sync-tidy.sh
+++ scripts/sync-tidy.sh
@@ -28,3 +28,12 @@ for example_dir in ${EXAMPLES_PATH}/*; do
     cd ${example_dir}
     go mod tidy
 done
+
+# Fail when changes are produced
+git add -A
+if git diff --cached --exit-code --quiet; then # --cached flag needed so new files are considered
+    echo "All go.mod files are synced and tidy."
+else
+    echo "Please run "make sync-tidy" locally to sync dependencies and tidy go.mod files. Commit the changes."
+    exit 1
+fi
