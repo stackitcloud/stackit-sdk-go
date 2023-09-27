892740730753c36bc61214d3e526813ad2b82236
vicentepinto98 <vicente.pinto@freiheit.com>
2023-09-27 11:18:33 +0100

    Update waiters on ObjectStorage to use core v0.2

--- services/objectstorage/wait.go
+++ services/objectstorage/wait.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/stackitcloud/stackit-sdk-go/core/utils"
+	oapiError "github.com/stackitcloud/stackit-sdk-go/core/oapierror"
 	"github.com/stackitcloud/stackit-sdk-go/core/wait"
 )
 
@@ -14,24 +14,12 @@ type APIClientBucketInterface interface {
 	GetBucketExecute(ctx context.Context, projectId string, bucketName string) (*GetBucketResponse, error)
 }
 
-func handleError(reqErr error) (res interface{}, done bool, err error) {
-	oapiErr, ok := reqErr.(*GenericOpenAPIError) //nolint:errorlint //complaining that error.As should be used to catch wrapped errors, but this error should not be wrapped
-	if !ok {
-		return nil, false, fmt.Errorf("could not convert error to GenericOpenApiError")
-	}
-	// Some APIs may return temporary errors and the request should be retried
-	if utils.Contains(wait.RetryHttpErrorStatusCodes, oapiErr.statusCode) {
-		return nil, false, nil
-	}
-	return nil, false, reqErr
-}
-
 // CreateBucketWaitHandler will wait for creation
 func CreateBucketWaitHandler(ctx context.Context, a APIClientBucketInterface, projectId, bucketName string) *wait.Handler {
 	waitHandler := wait.New(func() (res interface{}, done bool, err error) {
 		s, err := a.GetBucketExecute(ctx, projectId, bucketName)
 		if err != nil {
-			return handleError(err)
+			return nil, false, err
 		}
 		return s, true, nil
 	})
@@ -45,12 +33,12 @@ func DeleteBucketWaitHandler(ctx context.Context, a APIClientBucketInterface, pr
 		if err == nil {
 			return s, false, nil
 		}
-		oapiErr, ok := err.(*GenericOpenAPIError) //nolint:errorlint //complaining that error.As should be used to catch wrapped errors, but this error should not be wrapped
+		oapiErr, ok := err.(*oapiError.GenericOpenAPIError) //nolint:errorlint //complaining that error.As should be used to catch wrapped errors, but this error should not be wrapped
 		if !ok {
 			return nil, false, fmt.Errorf("could not convert error to GenericOpenApiError")
 		}
-		if oapiErr.statusCode != http.StatusNotFound {
-			return handleError(err)
+		if oapiErr.StatusCode != http.StatusNotFound {
+			return nil, false, err
 		}
 		return nil, true, nil
 	})
--- services/objectstorage/wait_test.go
+++ services/objectstorage/wait_test.go
@@ -2,12 +2,11 @@ package objectstorage
 
 import (
 	"context"
-	"fmt"
-	"net/http"
 	"testing"
 	"time"
 
 	"github.com/google/go-cmp/cmp"
+	oapiError "github.com/stackitcloud/stackit-sdk-go/core/oapierror"
 )
 
 // Used for testing bucket operations
@@ -18,78 +17,18 @@ type apiClientBucketMocked struct {
 
 func (a *apiClientBucketMocked) GetBucketExecute(_ context.Context, _, _ string) (*GetBucketResponse, error) {
 	if a.bucketGetFails {
-		return nil, &GenericOpenAPIError{
-			statusCode: 500,
+		return nil, &oapiError.GenericOpenAPIError{
+			StatusCode: 500,
 		}
 	}
 
 	if a.bucketIsDeleted {
-		return nil, &GenericOpenAPIError{
-			statusCode: 404,
+		return nil, &oapiError.GenericOpenAPIError{
+			StatusCode: 404,
 		}
 	}
 
 	return &GetBucketResponse{}, nil
-}
-
-func TestHandleError(t *testing.T) {
-	tests := []struct {
-		desc     string
-		reqErr   error
-		wantRes  interface{}
-		wantDone bool
-		wantErr  bool
-	}{
-		{
-			desc: "handle_oapi_error",
-			reqErr: &GenericOpenAPIError{
-				statusCode: 500,
-			},
-			wantRes:  nil,
-			wantDone: false,
-			wantErr:  true,
-		},
-		{
-			desc:     "not_generic_oapi_error",
-			reqErr:   fmt.Errorf("some error"),
-			wantRes:  nil,
-			wantDone: false,
-			wantErr:  true,
-		},
-		{
-			desc: "bad_gateway_error",
-			reqErr: &GenericOpenAPIError{
-				statusCode: http.StatusBadGateway,
-			},
-			wantRes:  nil,
-			wantDone: false,
-			wantErr:  false,
-		},
-		{
-			desc: "gateway_timeout_error",
-			reqErr: &GenericOpenAPIError{
-				statusCode: http.StatusBadGateway,
-			},
-			wantRes:  nil,
-			wantDone: false,
-			wantErr:  false,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.desc, func(t *testing.T) {
-			gotRes, gotDone, err := handleError(tt.reqErr)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("handleError() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !cmp.Equal(gotRes, tt.wantRes) {
-				t.Errorf("handleError() gotRes = %v, want %v", gotRes, tt.wantRes)
-			}
-			if gotDone != tt.wantDone {
-				t.Errorf("handleError() gotDone = %v, want %v", gotDone, tt.wantDone)
-			}
-		})
-	}
 }
 
 func TestCreateBucketWaitHandler(t *testing.T) {
