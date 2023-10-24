622b031e9fdd22a9872177a3447ebb7c11965034
Henrique Santos <118177985+hcsa73@users.noreply.github.com>
2023-10-24 16:47:22 +0100

    Add Service Account example (#124)
    * Add example
    
    * Update version
    
    * Remove hardcoded credentials
    
    ---------
    
    Co-authored-by: Henrique Santos <henrique.santos@freiheit.com>

---
+++ examples/serviceaccount/go.mod
@@ -0,0 +1,23 @@
+module github.com/stackitcloud/stackit-sdk-go/examples/serviceaccount
+
+go 1.18
+
+require (
+	github.com/stackitcloud/stackit-sdk-go/core v0.3.0
+	github.com/stackitcloud/stackit-sdk-go/services/serviceaccount v0.1.0
+)
+
+require (
+	github.com/MicahParks/keyfunc/v2 v2.1.0 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.1 // indirect
+	golang.org/x/net v0.16.0 // indirect
+	golang.org/x/oauth2 v0.13.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+	k8s.io/apimachinery v0.28.2 // indirect
+	k8s.io/klog/v2 v2.100.1 // indirect
+	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
+)
---
+++ examples/serviceaccount/serviceaccount.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/stackitcloud/stackit-sdk-go/core/utils"
+	"github.com/stackitcloud/stackit-sdk-go/services/serviceaccount"
+)
+
+func main() {
+	// Specify the project ID
+	projectId := "PROJECT_ID"
+
+	// Create a new API client, that uses default authentication and configuration
+	client, err := serviceaccount.NewAPIClient()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Creating API client: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Get the service accounts for your project
+	getAccountsResp, err := client.GetServiceAccounts(context.Background(), projectId).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `GetServiceAccounts`: %v\n", err)
+	} else {
+		fmt.Printf("Number of service accounts: %v\n", len(*getAccountsResp.Items))
+	}
+
+	// Create a service account
+	createAccountPayload := serviceaccount.CreateServiceAccountPayload{
+		Name: utils.Ptr("my-service-account"),
+	}
+	createAccountResp, err := client.CreateServiceAccount(context.Background(), projectId).CreateServiceAccountPayload(createAccountPayload).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `CreateInstance`: %v\n", err)
+	} else {
+		fmt.Printf("Created service account with email \"%s\".\n", *createAccountResp.Email)
+	}
+}
