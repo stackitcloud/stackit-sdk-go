f81d1911f4965839896fd4161f9b609e7d4a9048
vicentepinto98 <vicente.pinto@freiheit.com>
2023-10-31 16:41:09 +0000

    Update example

--- examples/membership/membership.go
+++ examples/membership/membership.go
@@ -59,8 +59,8 @@ func main() {
 		fmt.Printf("Number of members: %v\n", len(*getMembersResp.Members))
 	}
 
-	// Add a member to your project
-	createAccountPayload := membership.UpdateMembersPayload{
+	// Add a member to your project or add an additional role to an existing member
+	updateMemberPayload := membership.UpdateMembersPayload{
 		Members: &[]membership.Member{
 			{
 				Role:    utils.Ptr("project.member"),
@@ -69,10 +69,27 @@ func main() {
 		},
 		ResourceType: utils.Ptr("project"),
 	}
-	_, err = client.UpdateMembers(context.Background(), projectId).UpdateMembersPayload(createAccountPayload).Execute()
+	_, err = client.UpdateMembers(context.Background(), projectId).UpdateMembersPayload(updateMemberPayload).Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `UpdateMembers`: %v\n", err)
 	} else {
 		fmt.Printf("Added member successfully.\n")
+	}
+
+	// Remove a role from a member of your project
+	deleteMemberPayload := membership.DeleteMembersPayload{
+		Members: &[]membership.Member{
+			{
+				Role:    utils.Ptr("project.member"),
+				Subject: utils.Ptr(emailToBeAdded),
+			},
+		},
+		ResourceType: utils.Ptr("project"),
+	}
+	_, err = client.DeleteMembers(context.Background(), projectId).DeleteMembersPayload(deleteMemberPayload).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `DeleteMembers`: %v\n", err)
+	} else {
+		fmt.Printf("Removed member successfully.\n")
 	}
 }
