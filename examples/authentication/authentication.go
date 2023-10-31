973090100a630f418c9e729aca377715812bf9d3
vicentepinto98 <vicente.pinto@freiheit.com>
2023-10-31 11:51:34 +0000

    Add membership example!

--- examples/authentication/authentication.go
+++ examples/authentication/authentication.go
@@ -18,33 +18,36 @@ func main() {
 	// If the key flow cannot be used, it will try to find a token in the STACKIT_SERVICE_ACCOUNT_TOKEN. If not present, it will
 	// search in the credentials file. If the token is found, the TokenAuth flow is used.
 	// In case no authentication flow can be configured, the creation of a new client fails.
-	_, err := dns.NewAPIClient()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[DNS API] Creating API client: %v\n", err)
-		os.Exit(1)
-	}
+	// _, err := dns.NewAPIClient()
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "[DNS API] Creating API client: %v\n", err)
+	// 	os.Exit(1)
+	// }
 
-	// Create an unauthenticated API client
-	_, err = dns.NewAPIClient(config.WithoutAuthentication())
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[DNS API] Creating API client: %v\n", err)
-		os.Exit(1)
-	}
+	// // Create an unauthenticated API client
+	// _, err = dns.NewAPIClient(config.WithoutAuthentication())
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "[DNS API] Creating API client: %v\n", err)
+	// 	os.Exit(1)
+	// }
 
-	// Create a new API client, that will authenticate using the provided bearer token
-	token := "TOKEN"
-	_, err = dns.NewAPIClient(config.WithToken(token))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "[DNS API] Creating API client: %v\n", err)
-		os.Exit(1)
-	}
+	// // Create a new API client, that will authenticate using the provided bearer token
+	// token := "TOKEN"
+	// _, err = dns.NewAPIClient(config.WithToken(token))
+	// if err != nil {
+	// 	fmt.Fprintf(os.Stderr, "[DNS API] Creating API client: %v\n", err)
+	// 	os.Exit(1)
+	// }
 
 	// Create a new API client, that will authenticate using the key flow
-	saKeyPath := "/path/to/service_account_key.json"
-	privateKeyPath := "/path/to/private.key"
+	saKeyPath := "/home/vicentepinto/DevTools/sa_key.json"
+	privateKeyPath := "/home/vicentepinto/DevTools/private.key"
 	dnsClient, err := dns.NewAPIClient(
 		config.WithServiceAccountKeyPath(saKeyPath),
 		config.WithPrivateKeyPath(privateKeyPath),
+		config.WithJWKSEndpoint("https://api-qa.stackit.cloud/service-account/.well-known/jwks.json"),
+		config.WithTokenEndpoint("https://api-qa.stackit.cloud/service-account/token"),
+		config.WithEndpoint("https://dns.api.stg.stackit.cloud"),
 	)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "[DNS API] Creating API client: %v\n", err)
@@ -52,7 +55,7 @@ func main() {
 	}
 
 	// Check that you can make an authenticated request
-	getZoneResp, err := dnsClient.GetZones(context.Background(), "PROJECT_ID").Execute()
+	getZoneResp, err := dnsClient.GetZones(context.Background(), "c4da6a51-7453-4bbf-bef7-c163ec376e7d").Execute()
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "[DNS API] Error when calling `ZoneApi.GetZones`: %v\n", err)
