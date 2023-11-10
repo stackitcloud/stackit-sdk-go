aad8253f3e35fc3d4e210afced82c7957a622d2e
João Palet <joao.palet@outlook.com>
2023-11-10 11:46:58 +0000

    Add example for HTTP response capturing

---
+++ examples/httpresponse/httpresponse.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/stackitcloud/stackit-sdk-go/core/config"
+	"github.com/stackitcloud/stackit-sdk-go/services/mongodbflex"
+)
+
+func main() {
+	// Specify the project ID
+	projectId := "PROJECT_ID"
+
+	// Create a new API client, that uses default authentication and configuration
+	mongodbflexClient, err := mongodbflex.NewAPIClient(
+		config.WithRegion("eu01"),
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Creating API client: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Get the MongoDB Flex instances for your project and capture the HTTP response using the context
+	var httpResp *http.Response
+	ctxWithHTTPResp := config.WithCaptureHTTPResponse(context.Background(), &httpResp)
+	getInstancesResp, err := mongodbflexClient.GetInstances(ctxWithHTTPResp, projectId).Tag("tag").Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `GetInstances`: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Number of instances: %v\n\n", len(*getInstancesResp.Items))
+	fmt.Printf("HTTP response: %+v\n", *httpResp)
+}
