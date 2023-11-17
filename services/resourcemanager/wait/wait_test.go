a997838a128e1302a5f3e670038bf7458bf3b547
João Palet <joao.palet@outlook.com>
2023-11-17 15:37:12 +0000

    Adjust sleep before wait for resource manager unit test

--- services/resourcemanager/wait/wait_test.go
+++ services/resourcemanager/wait/wait_test.go
@@ -91,7 +91,7 @@ func TestCreateProjectWaitHandler(t *testing.T) {
 
 			handler := CreateProjectWaitHandler(context.Background(), apiClient, "cid")
 
-			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).SetSleepBeforeWait(10 * time.Millisecond).WaitWithContext(context.Background())
 
 			if (err != nil) != tt.wantErr {
 				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
