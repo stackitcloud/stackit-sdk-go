230aed2eec9b9a6857260603fbb627b84e389d5c
Henrique Santos <henrique.santos@freiheit.com>
2024-02-23 12:58:59 +0000

    Add example

---
+++ examples/backgroundrefresh/backgroundrefresh.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/stackitcloud/stackit-sdk-go/core/config"
+	"github.com/stackitcloud/stackit-sdk-go/services/dns"
+)
+
+func main() {
+	// Specify the project ID
+	projectId := "PROJECT_ID"
+
+	// Create a cancellable context
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel() // This is needed to abort the background routine, to prevent resource leaks
+
+	// Create a new API client, that uses key flow and has background token refresh enabled
+	dnsClient, err := dns.NewAPIClient(
+		config.WithServiceAccountKeyPath("path/to/service/account"),
+		config.WithBackgroundTokenRefresh(ctx),
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when creating API client: %v\n", err)
+	}
+
+	// The client can then be used as normal
+
+	// Get the DNS zones for your project
+	getZoneResp, err := dnsClient.ListZones(context.Background(), projectId).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error when calling `ListZones`: %v\n", err)
+	}
+	fmt.Printf("Number of DNS zones: %v\n", len(*getZoneResp.Zones))
+}
---
+++ examples/backgroundrefresh/go.mod
@@ -0,0 +1,14 @@
+module github.com/stackitcloud/stackit-sdk-go/examples/backgroundrefresh
+
+go 1.18
+
+require (
+	github.com/stackitcloud/stackit-sdk-go/core v0.9.0
+	github.com/stackitcloud/stackit-sdk-go/services/dns v0.8.3
+)
+
+require (
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+)
