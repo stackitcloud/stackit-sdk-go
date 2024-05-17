17cce43273f8b323b3c4ecc201667101f50661d5
João Palet <joao.palet@freiheit.com>
2024-05-17 13:34:10 +0100

    Add waiters for network

--- services/iaas/wait/wait.go
+++ services/iaas/wait/wait.go
@@ -18,7 +18,8 @@ const (
 // Interfaces needed for tests
 type APIClientInterface interface {
 	GetNetworkAreaExecute(ctx context.Context, organizationId, areaId string) (*iaas.NetworkArea, error)
-	// GetNetworkExecute(ctx context.Context, projectId, networkId string) (*iaas.Network, error)
+	GetProjectRequestExecute(ctx context.Context, projectId string, requestId string) (*iaas.Request, error)
+	GetNetworkExecute(ctx context.Context, projectId, networkId string) (*iaas.Network, error)
 }
 
 // CreateNetworkAreaWaitHandler will wait for network area creation
@@ -65,12 +66,84 @@ func UpdateNetworkAreaWaitHandler(ctx context.Context, a APIClientInterface, org
 func DeleteNetworkAreaWaitHandler(ctx context.Context, a APIClientInterface, organizationId, areaId string) *wait.AsyncActionHandler[iaas.NetworkArea] {
 	handler := wait.New(func() (waitFinished bool, response *iaas.NetworkArea, err error) {
 		area, err := a.GetNetworkAreaExecute(ctx, organizationId, areaId)
+		if err == nil {
+			return false, nil, nil
+		}
 		oapiErr, ok := err.(*oapierror.GenericOpenAPIError) //nolint:errorlint //complaining that error.As should be used to catch wrapped errors, but this error should not be wrapped
 		if !ok {
-			return false, area, fmt.Errorf("could not convert error to oapierror.GenericOpenAPIError")
+			return false, area, fmt.Errorf("could not convert error to oapierror.GenericOpenAPIError: %w", err)
 		}
 		if oapiErr.StatusCode != http.StatusNotFound {
 			return false, area, err
+		}
+		return true, nil, nil
+	})
+	handler.SetTimeout(10 * time.Minute)
+	return handler
+}
+
+// CreateNetworkWaitHandler will wait for network creation
+func CreateNetworkWaitHandler(ctx context.Context, a APIClientInterface, projectId, requestId string) *wait.AsyncActionHandler[iaas.Network] {
+	handler := wait.New(func() (waitFinished bool, response *iaas.Network, err error) {
+		request, err := a.GetProjectRequestExecute(ctx, projectId, requestId)
+		if err != nil {
+			return false, nil, fmt.Errorf("get request with id %s", requestId)
+		}
+		if request == nil || request.Resources == nil || len(*request.Resources) == 0 || (*request.Resources)[0].Id == nil {
+			return false, nil, fmt.Errorf("no resources found for request with id %s", requestId)
+		}
+		networkId := *(*request.Resources)[0].Id
+		network, err := a.GetNetworkExecute(ctx, projectId, networkId)
+		if err != nil {
+			return false, network, fmt.Errorf("get network with id %s", networkId)
+		}
+		if network.NetworkId == nil || network.State == nil {
+			return false, network, fmt.Errorf("the response is not valid: the id or the state are missing")
+		}
+		if *network.NetworkId == networkId && *network.State == CreateSuccess {
+			return true, network, nil
+		}
+		return false, network, nil
+	})
+	handler.SetSleepBeforeWait(2 * time.Second)
+	handler.SetTimeout(10 * time.Minute)
+	return handler
+}
+
+// UpdateNetworkWaitHandler will wait for network update
+func UpdateNetworkWaitHandler(ctx context.Context, a APIClientInterface, projectId, networkId string) *wait.AsyncActionHandler[iaas.Network] {
+	handler := wait.New(func() (waitFinished bool, response *iaas.Network, err error) {
+		network, err := a.GetNetworkExecute(ctx, projectId, networkId)
+		if err != nil {
+			return false, network, err
+		}
+		if network.NetworkId == nil || network.State == nil {
+			return false, network, fmt.Errorf("the response is not valid: the id or the state are missing")
+		}
+		// The state returns to "CREATED" after a successful update is completed
+		if *network.NetworkId == networkId && *network.State == CreateSuccess {
+			return true, network, nil
+		}
+		return false, network, nil
+	})
+	handler.SetSleepBeforeWait(2 * time.Second)
+	handler.SetTimeout(10 * time.Minute)
+	return handler
+}
+
+// DeleteNetworkWaitHandler will wait for network deletion
+func DeleteNetworkWaitHandler(ctx context.Context, a APIClientInterface, projectId, networkId string) *wait.AsyncActionHandler[iaas.Network] {
+	handler := wait.New(func() (waitFinished bool, response *iaas.Network, err error) {
+		network, err := a.GetNetworkExecute(ctx, projectId, networkId)
+		if err == nil {
+			return false, nil, nil
+		}
+		oapiErr, ok := err.(*oapierror.GenericOpenAPIError) //nolint:errorlint //complaining that error.As should be used to catch wrapped errors, but this error should not be wrapped
+		if !ok {
+			return false, network, fmt.Errorf("could not convert error to oapierror.GenericOpenAPIError: %w", err)
+		}
+		if oapiErr.StatusCode != http.StatusNotFound {
+			return false, network, err
 		}
 		return true, nil, nil
 	})
--- services/iaas/wait/wait_test.go
+++ services/iaas/wait/wait_test.go
@@ -12,9 +12,12 @@ import (
 )
 
 type apiClientMocked struct {
-	getFails      bool
-	isDeleted     bool
-	resourceState string
+	getNetworkAreaFails    bool
+	getNetworkFails        bool
+	getProjectRequestFails bool
+	isDeleted              bool
+	resourceState          string
+	getProjectRequestResp  *iaas.Request
 }
 
 func (a *apiClientMocked) GetNetworkAreaExecute(_ context.Context, _, _ string) (*iaas.NetworkArea, error) {
@@ -24,7 +27,7 @@ func (a *apiClientMocked) GetNetworkAreaExecute(_ context.Context, _, _ string) 
 		}
 	}
 
-	if a.getFails {
+	if a.getNetworkAreaFails {
 		return nil, &oapierror.GenericOpenAPIError{
 			StatusCode: 500,
 		}
@@ -34,6 +37,35 @@ func (a *apiClientMocked) GetNetworkAreaExecute(_ context.Context, _, _ string) 
 		AreaId: utils.Ptr("naid"),
 		State:  &a.resourceState,
 	}, nil
+}
+
+func (a *apiClientMocked) GetNetworkExecute(_ context.Context, _, _ string) (*iaas.Network, error) {
+	if a.isDeleted {
+		return nil, &oapierror.GenericOpenAPIError{
+			StatusCode: 404,
+		}
+	}
+
+	if a.getNetworkFails {
+		return nil, &oapierror.GenericOpenAPIError{
+			StatusCode: 500,
+		}
+	}
+
+	return &iaas.Network{
+		NetworkId: utils.Ptr("nid"),
+		State:     &a.resourceState,
+	}, nil
+}
+
+func (a *apiClientMocked) GetProjectRequestExecute(_ context.Context, _, _ string) (*iaas.Request, error) {
+	if a.getProjectRequestFails {
+		return nil, &oapierror.GenericOpenAPIError{
+			StatusCode: 500,
+		}
+	}
+
+	return a.getProjectRequestResp, nil
 }
 
 func TestCreateNetworkAreaWaitHandler(t *testing.T) {
@@ -69,8 +101,8 @@ func TestCreateNetworkAreaWaitHandler(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
 			apiClient := &apiClientMocked{
-				getFails:      tt.getFails,
-				resourceState: tt.resourceState,
+				getNetworkAreaFails: tt.getFails,
+				resourceState:       tt.resourceState,
 			}
 
 			var wantRes *iaas.NetworkArea
@@ -128,8 +160,8 @@ func TestUpdateNetworkAreaWaitHandler(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
 			apiClient := &apiClientMocked{
-				getFails:      tt.getFails,
-				resourceState: tt.resourceState,
+				getNetworkAreaFails: tt.getFails,
+				resourceState:       tt.resourceState,
 			}
 
 			var wantRes *iaas.NetworkArea
@@ -142,7 +174,7 @@ func TestUpdateNetworkAreaWaitHandler(t *testing.T) {
 
 			handler := UpdateNetworkAreaWaitHandler(context.Background(), apiClient, "oid", "naid")
 
-			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).SetSleepBeforeWait(1 * time.Millisecond).WaitWithContext(context.Background())
 
 			if (err != nil) != tt.wantErr {
 				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
@@ -188,9 +220,9 @@ func TestDeleteNetworkAreaWaitHandler(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.desc, func(t *testing.T) {
 			apiClient := &apiClientMocked{
-				getFails:      tt.getFails,
-				isDeleted:     tt.isDeleted,
-				resourceState: tt.resourceState,
+				getNetworkAreaFails: tt.getFails,
+				isDeleted:           tt.isDeleted,
+				resourceState:       tt.resourceState,
 			}
 
 			var wantRes *iaas.NetworkArea
@@ -202,6 +234,248 @@ func TestDeleteNetworkAreaWaitHandler(t *testing.T) {
 			}
 
 			handler := DeleteNetworkAreaWaitHandler(context.Background(), apiClient, "oid", "naid")
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		})
+	}
+}
+
+func TestCreateNetworkWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc                   string
+		getProjectRequestFails bool
+		getProjectRequestResp  *iaas.Request
+		getNetworkFails        bool
+		resourceState          string
+		wantErr                bool
+		wantResp               bool
+	}{
+		{
+			desc:                   "create_succeeded",
+			getProjectRequestFails: false,
+			getProjectRequestResp: &iaas.Request{
+				Resources: &[]iaas.RequestResource{
+					{
+						Id: utils.Ptr("nid"),
+					},
+				},
+			},
+			getNetworkFails: false,
+			resourceState:   CreateSuccess,
+			wantErr:         false,
+			wantResp:        true,
+		},
+		{
+			desc:                   "get_request_fails",
+			getProjectRequestFails: true,
+			getProjectRequestResp:  nil,
+			getNetworkFails:        false,
+			resourceState:          "",
+			wantErr:                true,
+			wantResp:               false,
+		},
+		{
+			desc:                   "get_network_fails",
+			getProjectRequestFails: false,
+			getProjectRequestResp: &iaas.Request{
+				Resources: &[]iaas.RequestResource{
+					{
+						Id: utils.Ptr("nid"),
+					},
+				},
+			},
+			getNetworkFails: true,
+			resourceState:   "",
+			wantErr:         true,
+			wantResp:        false,
+		},
+		{
+			desc:                   "timeout",
+			getProjectRequestFails: false,
+			getProjectRequestResp: &iaas.Request{
+				Resources: &[]iaas.RequestResource{
+					{
+						Id: utils.Ptr("nid"),
+					},
+				},
+			},
+			getNetworkFails: false,
+			resourceState:   "ANOTHER STATE",
+			wantErr:         true,
+			wantResp:        true,
+		},
+		{
+			desc:                   "no_resources",
+			getProjectRequestFails: false,
+			getProjectRequestResp: &iaas.Request{
+				Resources: &[]iaas.RequestResource{},
+			},
+			getNetworkFails: false,
+			resourceState:   "",
+			wantErr:         true,
+			wantResp:        false,
+		},
+		{
+			desc:                   "no_resource_id_in_request",
+			getProjectRequestFails: false,
+			getProjectRequestResp: &iaas.Request{
+				Resources: &[]iaas.RequestResource{
+					{
+						Id: nil,
+					},
+				},
+			},
+			getNetworkFails: false,
+			resourceState:   "",
+			wantErr:         true,
+			wantResp:        false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			apiClient := &apiClientMocked{
+				getProjectRequestFails: tt.getProjectRequestFails,
+				getProjectRequestResp:  tt.getProjectRequestResp,
+				getNetworkFails:        tt.getNetworkFails,
+				resourceState:          tt.resourceState,
+			}
+
+			var wantRes *iaas.Network
+			if tt.wantResp {
+				wantRes = &iaas.Network{
+					NetworkId: utils.Ptr("nid"),
+					State:     &tt.resourceState,
+				}
+			}
+
+			handler := CreateNetworkWaitHandler(context.Background(), apiClient, "pid", "rid")
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).SetSleepBeforeWait(1 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		})
+	}
+}
+
+func TestUpdateNetworkWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc          string
+		getFails      bool
+		resourceState string
+		wantErr       bool
+		wantResp      bool
+	}{
+		{
+			desc:          "update_succeeded",
+			getFails:      false,
+			resourceState: CreateSuccess,
+			wantErr:       false,
+			wantResp:      true,
+		},
+		{
+			desc:          "get_fails",
+			getFails:      true,
+			resourceState: "",
+			wantErr:       true,
+			wantResp:      false,
+		},
+		{
+			desc:          "timeout",
+			getFails:      false,
+			resourceState: "ANOTHER STATE",
+			wantErr:       true,
+			wantResp:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			apiClient := &apiClientMocked{
+				getNetworkFails: tt.getFails,
+				resourceState:   tt.resourceState,
+			}
+
+			var wantRes *iaas.Network
+			if tt.wantResp {
+				wantRes = &iaas.Network{
+					NetworkId: utils.Ptr("nid"),
+					State:     &tt.resourceState,
+				}
+			}
+
+			handler := UpdateNetworkWaitHandler(context.Background(), apiClient, "pid", "nid")
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).SetSleepBeforeWait(1 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		})
+	}
+}
+
+func TestDeleteNetworkWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc          string
+		getFails      bool
+		isDeleted     bool
+		resourceState string
+		wantErr       bool
+		wantResp      bool
+	}{
+		{
+			desc:      "delete_succeeded",
+			getFails:  false,
+			isDeleted: true,
+			wantErr:   false,
+			wantResp:  false,
+		},
+		{
+			desc:          "get_fails",
+			getFails:      true,
+			resourceState: "",
+			wantErr:       true,
+			wantResp:      false,
+		},
+		{
+			desc:          "timeout",
+			getFails:      false,
+			resourceState: "ANOTHER STATE",
+			wantErr:       true,
+			wantResp:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			apiClient := &apiClientMocked{
+				getNetworkFails: tt.getFails,
+				isDeleted:       tt.isDeleted,
+				resourceState:   tt.resourceState,
+			}
+
+			var wantRes *iaas.Network
+			if tt.wantResp {
+				wantRes = &iaas.Network{
+					NetworkId: utils.Ptr("nid"),
+					State:     &tt.resourceState,
+				}
+			}
+
+			handler := DeleteNetworkWaitHandler(context.Background(), apiClient, "pid", "nid")
 
 			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
 
