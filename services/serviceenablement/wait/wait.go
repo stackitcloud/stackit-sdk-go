bf291f2f3413990adf0ee16dde6b8a81a27c68c9
Diogo Ferrão <diogo.ferrao@freiheit.com>
2024-07-12 16:46:48 +0100

    Add waiters for service enablement EnableService and DisableService

---
+++ services/serviceenablement/wait/wait.go
@@ -0,0 +1,74 @@
+package wait
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/stackitcloud/stackit-sdk-go/core/wait"
+	"github.com/stackitcloud/stackit-sdk-go/services/serviceenablement"
+)
+
+const (
+	ServiceStateEnabled   = "ENABLED"
+	ServiceStateEnabling  = "ENABLING"
+	ServiceStateDisabled  = "DISABLED"
+	ServiceStateDisabling = "DISABLING"
+)
+
+// Interface needed for tests
+type APIClientInstanceInterface interface {
+	GetServiceStatusExecute(ctx context.Context, projectId, serviceId string) (*serviceenablement.ServiceStatus, error)
+}
+
+func EnableServiceWaitHandler(ctx context.Context, a APIClientInstanceInterface, projectId, serviceId string) *wait.AsyncActionHandler[serviceenablement.ServiceStatus] {
+	handler := wait.New(func() (waitFinished bool, response *serviceenablement.ServiceStatus, err error) {
+		s, err := a.GetServiceStatusExecute(ctx, projectId, serviceId)
+		if err != nil {
+			return false, nil, err
+		}
+		if s == nil || s.State == nil {
+			return false, nil, nil
+		}
+		switch *s.State {
+		default:
+			return true, s, fmt.Errorf("service with id %s has unexpected state %s", serviceId, *s.State)
+		case ServiceStateEnabled:
+			return true, s, nil
+		case ServiceStateEnabling:
+			return false, nil, nil
+		case ServiceStateDisabled:
+			return true, s, fmt.Errorf("enabling failed for service with id %s", serviceId)
+		case ServiceStateDisabling:
+			return true, s, fmt.Errorf("service with id %s is in state %s", serviceId, *s.State)
+		}
+	})
+	handler.SetTimeout(45 * time.Minute)
+	return handler
+}
+
+func DisableServiceWaitHandler(ctx context.Context, a APIClientInstanceInterface, projectId, serviceId string) *wait.AsyncActionHandler[serviceenablement.ServiceStatus] {
+	handler := wait.New(func() (waitFinished bool, response *serviceenablement.ServiceStatus, err error) {
+		s, err := a.GetServiceStatusExecute(ctx, projectId, serviceId)
+		if err != nil {
+			return false, nil, err
+		}
+		if s == nil || s.State == nil {
+			return false, nil, nil
+		}
+		switch *s.State {
+		default:
+			return true, s, fmt.Errorf("service with id %s has unexpected state %s", serviceId, *s.State)
+		case ServiceStateDisabled:
+			return true, s, nil
+		case ServiceStateDisabling:
+			return false, nil, nil
+		case ServiceStateEnabled:
+			return true, s, fmt.Errorf("disabling failed for service with id %s", serviceId)
+		case ServiceStateEnabling:
+			return true, s, fmt.Errorf("service with id %s is in state %s", serviceId, *s.State)
+		}
+	})
+	handler.SetTimeout(45 * time.Minute)
+	return handler
+}
---
+++ services/serviceenablement/wait/wait_test.go
@@ -0,0 +1,195 @@
+package wait
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/stackitcloud/stackit-sdk-go/core/oapierror"
+	"github.com/stackitcloud/stackit-sdk-go/services/serviceenablement"
+)
+
+// Used for testing service operations
+type apiClientServiceMocked struct {
+	serviceId       string
+	serviceState    string
+	getServiceFails bool
+}
+
+func (a *apiClientServiceMocked) GetServiceStatusExecute(_ context.Context, _, _ string) (*serviceenablement.ServiceStatus, error) {
+	if a.getServiceFails {
+		return nil, &oapierror.GenericOpenAPIError{
+			StatusCode: 500,
+		}
+	}
+
+	return &serviceenablement.ServiceStatus{
+		ServiceId: &a.serviceId,
+		State:     &a.serviceState,
+	}, nil
+}
+
+func TestEnableServiceWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc            string
+		getServiceFails bool
+		serviceState    string
+		wantErr         bool
+		wantResp        bool
+	}{
+		{
+			desc:            "enable_succeeded",
+			getServiceFails: false,
+			serviceState:    ServiceStateEnabled,
+			wantErr:         false,
+			wantResp:        true,
+		},
+		{
+			desc:            "enable_failed",
+			getServiceFails: false,
+			serviceState:    ServiceStateDisabled,
+			wantErr:         true,
+			wantResp:        false,
+		},
+		{
+			desc:            "enable_failed_2",
+			getServiceFails: false,
+			serviceState:    ServiceStateDisabling,
+			wantErr:         true,
+			wantResp:        false,
+		},
+		{
+			desc:            "timeout",
+			getServiceFails: false,
+			serviceState:    ServiceStateEnabling,
+			wantErr:         true,
+			wantResp:        false,
+		},
+		{
+			desc:            "get_service_fails",
+			getServiceFails: true,
+			serviceState:    ServiceStateEnabling,
+			wantErr:         true,
+			wantResp:        false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			serviceId := "serviceId"
+
+			apiClient := &apiClientServiceMocked{
+				serviceId:       serviceId,
+				serviceState:    tt.serviceState,
+				getServiceFails: tt.getServiceFails,
+			}
+
+			var wantRes *serviceenablement.ServiceStatus
+			if tt.wantResp {
+				wantRes = &serviceenablement.ServiceStatus{
+					ServiceId: &serviceId,
+					State:     &tt.serviceState,
+				}
+			}
+
+			ctx := context.Background()
+
+			handler := EnableServiceWaitHandler(ctx, apiClient, "projectId", serviceId)
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(ctx)
+
+			if err != nil {
+				if !tt.wantErr {
+					t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		},
+		)
+	}
+}
+
+func TestDisableServiceWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc            string
+		getServiceFails bool
+		serviceState    string
+		wantErr         bool
+		wantResp        bool
+	}{
+		{
+			desc:            "disable_succeeded",
+			getServiceFails: false,
+			serviceState:    ServiceStateDisabled,
+			wantErr:         false,
+			wantResp:        true,
+		},
+		{
+			desc:            "disable_failed",
+			getServiceFails: false,
+			serviceState:    ServiceStateEnabled,
+			wantErr:         true,
+			wantResp:        false,
+		},
+		{
+			desc:            "disable_failed_2",
+			getServiceFails: false,
+			serviceState:    ServiceStateEnabling,
+			wantErr:         true,
+			wantResp:        false,
+		},
+		{
+			desc:            "timeout",
+			getServiceFails: false,
+			serviceState:    ServiceStateDisabling,
+			wantErr:         true,
+			wantResp:        false,
+		},
+		{
+			desc:            "get_service_fails",
+			getServiceFails: true,
+			serviceState:    ServiceStateDisabling,
+			wantErr:         true,
+			wantResp:        false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			serviceId := "serviceId"
+
+			apiClient := &apiClientServiceMocked{
+				serviceId:       serviceId,
+				serviceState:    tt.serviceState,
+				getServiceFails: tt.getServiceFails,
+			}
+
+			var wantRes *serviceenablement.ServiceStatus
+			if tt.wantResp {
+				wantRes = &serviceenablement.ServiceStatus{
+					ServiceId: &serviceId,
+					State:     &tt.serviceState,
+				}
+			}
+
+			ctx := context.Background()
+
+			handler := DisableServiceWaitHandler(ctx, apiClient, "projectId", serviceId)
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(ctx)
+
+			if err != nil {
+				if !tt.wantErr {
+					t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+				}
+				return
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		},
+		)
+	}
+}
