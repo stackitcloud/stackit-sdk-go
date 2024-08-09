462f4c2fdff241564a09bcd6eb55d40a35ff8cdf
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2024-08-09 08:15:28 +0200

    Generate runcommand (#686)

--- services/runcommand/model_command_template_response.go
+++ services/runcommand/model_command_template_response.go
@@ -10,6 +10,107 @@ API version: 1.0
 
 package runcommand
 
+import (
+	"encoding/json"
+)
+
+// checks if the CommandTemplateResponse type satisfies the MappedNullable interface at compile time
+var _ MappedNullable = &CommandTemplateResponse{}
+
+// CommandTemplateResponse struct for CommandTemplateResponse
 type CommandTemplateResponse struct {
 	Items *[]CommandTemplate `json:"items,omitempty"`
+}
+
+// NewCommandTemplateResponse instantiates a new CommandTemplateResponse object
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed
+func NewCommandTemplateResponse() *CommandTemplateResponse {
+	this := CommandTemplateResponse{}
+	return &this
+}
+
+// NewCommandTemplateResponseWithDefaults instantiates a new CommandTemplateResponse object
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set
+func NewCommandTemplateResponseWithDefaults() *CommandTemplateResponse {
+	this := CommandTemplateResponse{}
+	return &this
+}
+
+// GetItems returns the Items field value if set, zero value otherwise.
+func (o *CommandTemplateResponse) GetItems() *[]CommandTemplate {
+	if o == nil || IsNil(o.Items) {
+		var ret *[]CommandTemplate
+		return ret
+	}
+	return o.Items
+}
+
+// GetItemsOk returns a tuple with the Items field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *CommandTemplateResponse) GetItemsOk() (*[]CommandTemplate, bool) {
+	if o == nil || IsNil(o.Items) {
+		return nil, false
+	}
+	return o.Items, true
+}
+
+// HasItems returns a boolean if a field has been set.
+func (o *CommandTemplateResponse) HasItems() bool {
+	if o != nil && !IsNil(o.Items) {
+		return true
+	}
+
+	return false
+}
+
+// SetItems gets a reference to the given []CommandTemplate and assigns it to the Items field.
+func (o *CommandTemplateResponse) SetItems(v *[]CommandTemplate) {
+	o.Items = v
+}
+
+func (o CommandTemplateResponse) ToMap() (map[string]interface{}, error) {
+	toSerialize := map[string]interface{}{}
+	if !IsNil(o.Items) {
+		toSerialize["items"] = o.Items
+	}
+	return toSerialize, nil
+}
+
+type NullableCommandTemplateResponse struct {
+	value *CommandTemplateResponse
+	isSet bool
+}
+
+func (v NullableCommandTemplateResponse) Get() *CommandTemplateResponse {
+	return v.value
+}
+
+func (v *NullableCommandTemplateResponse) Set(val *CommandTemplateResponse) {
+	v.value = val
+	v.isSet = true
+}
+
+func (v NullableCommandTemplateResponse) IsSet() bool {
+	return v.isSet
+}
+
+func (v *NullableCommandTemplateResponse) Unset() {
+	v.value = nil
+	v.isSet = false
+}
+
+func NewNullableCommandTemplateResponse(val *CommandTemplateResponse) *NullableCommandTemplateResponse {
+	return &NullableCommandTemplateResponse{value: val, isSet: true}
+}
+
+func (v NullableCommandTemplateResponse) MarshalJSON() ([]byte, error) {
+	return json.Marshal(v.value)
+}
+
+func (v *NullableCommandTemplateResponse) UnmarshalJSON(src []byte) error {
+	v.isSet = true
+	return json.Unmarshal(src, &v.value)
 }
