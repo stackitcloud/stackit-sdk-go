3b45eb65af1dea0f00571d4274fbab7e870e7873
Vicente Pinto <vicente.pinto@freiheit.com>
2024-10-01 17:04:28 +0100

    Add server waiters

--- services/iaasalpha/wait/wait.go
+++ services/iaasalpha/wait/wait.go
@@ -15,11 +15,14 @@ const (
 	AvailableStatus = "AVAILABLE"
 	DeleteSuccess   = "DELETED"
 	ErrorStatus     = "ERROR"
+	ActiveStatus    = "ACTIVE"
+	ResizingStatus  = "RESIZING"
 )
 
 // Interfaces needed for tests
 type APIClientInterface interface {
 	GetVolumeExecute(ctx context.Context, projectId string, volumeId string) (*iaasalpha.Volume, error)
+	GetServerExecute(ctx context.Context, projectId string, serverId string) (*iaasalpha.Server, error)
 }
 
 // CreateVolumeWaitHandler will wait for volume creation
@@ -69,5 +72,105 @@ func DeleteVolumeWaitHandler(ctx context.Context, a APIClientInterface, projectI
 		return true, nil, nil
 	})
 	handler.SetTimeout(10 * time.Minute)
+	return handler
+}
+
+// CreateServerWaitHandler will wait for server creation
+func CreateServerWaitHandler(ctx context.Context, a APIClientInterface, projectId, serverId string) *wait.AsyncActionHandler[iaasalpha.Server] {
+	handler := wait.New(func() (waitFinished bool, response *iaasalpha.Server, err error) {
+		server, err := a.GetServerExecute(ctx, projectId, serverId)
+		if err != nil {
+			return false, server, err
+		}
+		if server.Id == nil || server.Status == nil {
+			return false, server, fmt.Errorf("create failed for server with id %s, the response is not valid: the id or the status are missing", serverId)
+		}
+		if *server.Id == serverId && *server.Status == ActiveStatus {
+			return true, server, nil
+		}
+		if *server.Id == serverId && *server.Status == ErrorStatus {
+			return true, server, fmt.Errorf("create failed for server with id %s", serverId)
+		}
+		return false, server, nil
+	})
+	handler.SetTimeout(20 * time.Minute)
+	return handler
+}
+
+// ResizingServerWaitHandler will wait for a server to be resizing
+// Ii is an intermediate step inside the ResizeServerWaitHandler
+func resizingServerWaitHandler(ctx context.Context, a APIClientInterface, projectId, serverId string) *wait.AsyncActionHandler[iaasalpha.Server] {
+	handler := wait.New(func() (waitFinished bool, response *iaasalpha.Server, err error) {
+		server, err := a.GetServerExecute(ctx, projectId, serverId)
+		if err != nil {
+			return false, server, err
+		}
+		if server.Id == nil || server.Status == nil {
+			return false, server, fmt.Errorf("resizing failed for server with id %s, the response is not valid: the id or the status are missing", serverId)
+		}
+		if *server.Id == serverId && *server.Status == ResizingStatus {
+			return true, server, nil
+		}
+		if *server.Id == serverId && *server.Status == ErrorStatus {
+			return true, server, fmt.Errorf("resizing failed for server with id %s", serverId)
+		}
+		return false, server, nil
+	})
+	handler.SetTimeout(10 * time.Minute)
+	return handler
+}
+
+// ResizeServerWaitHandler will wait for server resize
+// It checks for an intermediate resizing status and only then waits for the server to become active
+func ResizeServerWaitHandler(ctx context.Context, a APIClientInterface, projectId, serverId string) *wait.AsyncActionHandler[iaasalpha.Server] {
+	handler := wait.New(func() (waitFinished bool, response *iaasalpha.Server, err error) {
+		server, err := resizingServerWaitHandler(ctx, a, projectId, serverId).WaitWithContext(ctx)
+		if err != nil {
+			return false, server, err
+		}
+		server, err = a.GetServerExecute(ctx, projectId, serverId)
+		if err != nil {
+			return false, server, err
+		}
+		if server.Id == nil || server.Status == nil {
+			return false, server, fmt.Errorf("resizing failed for server with id %s, the response is not valid: the id or the status are missing", serverId)
+		}
+		if *server.Id == serverId && *server.Status == ActiveStatus {
+			return true, server, nil
+		}
+		if *server.Id == serverId && *server.Status == ErrorStatus {
+			return true, server, fmt.Errorf("resizing failed for server with id %s", serverId)
+		}
+		return false, server, nil
+	})
+	handler.SetTimeout(20 * time.Minute)
+	return handler
+}
+
+// DeleteServerWaitHandler will wait for volume deletion
+func DeleteServerWaitHandler(ctx context.Context, a APIClientInterface, projectId, serverId string) *wait.AsyncActionHandler[iaasalpha.Server] {
+	handler := wait.New(func() (waitFinished bool, response *iaasalpha.Server, err error) {
+		server, err := a.GetServerExecute(ctx, projectId, serverId)
+		if err == nil {
+			if server != nil {
+				if server.Id == nil || server.Status == nil {
+					return false, server, fmt.Errorf("delete failed for server with id %s, the response is not valid: the id or the status are missing", serverId)
+				}
+				if *server.Id == serverId && *server.Status == DeleteSuccess {
+					return true, server, nil
+				}
+			}
+			return false, nil, nil
+		}
+		oapiErr, ok := err.(*oapierror.GenericOpenAPIError) //nolint:errorlint //complaining that error.As should be used to catch wrapped errors, but this error should not be wrapped
+		if !ok {
+			return false, server, fmt.Errorf("could not convert error to oapierror.GenericOpenAPIError: %w", err)
+		}
+		if oapiErr.StatusCode != http.StatusNotFound {
+			return false, server, err
+		}
+		return true, nil, nil
+	})
+	handler.SetTimeout(20 * time.Minute)
 	return handler
 }
