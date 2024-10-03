b5e948521be3b94abc60e87f3749923d07ebc084
Vicente Pinto <vicente.pinto@freiheit.com>
2024-10-03 10:50:54 +0100

    IaaS Server waiters (#865)
    * Add server waiters
    
    * Add waiter unit tests
    
    * Fix litn
    
    * Rename variables

--- services/iaasalpha/wait/wait.go
+++ services/iaasalpha/wait/wait.go
@@ -12,14 +12,17 @@ import (
 )
 
 const (
-	AvailableStatus = "AVAILABLE"
-	DeleteSuccess   = "DELETED"
-	ErrorStatus     = "ERROR"
+	VolumeAvailableStatus = "AVAILABLE"
+	DeleteSuccess         = "DELETED"
+	ErrorStatus           = "ERROR"
+	ServerActiveStatus    = "ACTIVE"
+	ServerResizingStatus  = "RESIZING"
 )
 
 // Interfaces needed for tests
 type APIClientInterface interface {
 	GetVolumeExecute(ctx context.Context, projectId string, volumeId string) (*iaasalpha.Volume, error)
+	GetServerExecute(ctx context.Context, projectId string, serverId string) (*iaasalpha.Server, error)
 }
 
 // CreateVolumeWaitHandler will wait for volume creation
@@ -32,7 +35,7 @@ func CreateVolumeWaitHandler(ctx context.Context, a APIClientInterface, projectI
 		if volume.Id == nil || volume.Status == nil {
 			return false, volume, fmt.Errorf("create failed for volume with id %s, the response is not valid: the id or the status are missing", volumeId)
 		}
-		if *volume.Id == volumeId && *volume.Status == AvailableStatus {
+		if *volume.Id == volumeId && *volume.Status == VolumeAvailableStatus {
 			return true, volume, nil
 		}
 		if *volume.Id == volumeId && *volume.Status == ErrorStatus {
@@ -69,5 +72,105 @@ func DeleteVolumeWaitHandler(ctx context.Context, a APIClientInterface, projectI
 		return true, nil, nil
 	})
 	handler.SetTimeout(10 * time.Minute)
+	return handler
+}
+
+// CreateServerWaitHandler will wait for server creation
+func CreateServerWaitHandler(ctx context.Context, a APIClientInterface, projectId, serverId string) *wait.AsyncActionHandler[iaasalpha.Server] {
+	handler := wait.New(func() (waitFinished bool, response *iaasalpha.Server, err error) {
+		server, err := a.GetServerExecute(ctx, projectId, serverId)
+		if err != nil {
+			return false, server, err
+		}
+		if server.Id == nil || server.Status == nil {
+			return false, server, fmt.Errorf("create failed for server with id %s, the response is not valid: the id or the status are missing", serverId)
+		}
+		if *server.Id == serverId && *server.Status == ServerActiveStatus {
+			return true, server, nil
+		}
+		if *server.Id == serverId && *server.Status == ErrorStatus {
+			return true, server, fmt.Errorf("create failed for server with id %s", serverId)
+		}
+		return false, server, nil
+	})
+	handler.SetTimeout(20 * time.Minute)
+	return handler
+}
+
+// ResizingServerWaitHandler will wait for a server to be resizing
+// Ii is an intermediate step inside the ResizeServerWaitHandler
+func resizingServerWaitHandler(ctx context.Context, a APIClientInterface, projectId, serverId string) *wait.AsyncActionHandler[iaasalpha.Server] {
+	handler := wait.New(func() (waitFinished bool, response *iaasalpha.Server, err error) {
+		server, err := a.GetServerExecute(ctx, projectId, serverId)
+		if err != nil {
+			return false, server, err
+		}
+		if server.Id == nil || server.Status == nil {
+			return false, server, fmt.Errorf("resizing failed for server with id %s, the response is not valid: the id or the status are missing", serverId)
+		}
+		if *server.Id == serverId && *server.Status == ServerResizingStatus {
+			return true, server, nil
+		}
+		if *server.Id == serverId && *server.Status == ErrorStatus {
+			return true, server, fmt.Errorf("resizing failed for server with id %s", serverId)
+		}
+		return false, server, nil
+	})
+	handler.SetTimeout(10 * time.Minute)
+	return handler
+}
+
+// ResizeServerWaitHandler will wait for server resize
+// It checks for an intermediate resizing status and only then waits for the server to become active
+func ResizeServerWaitHandler(ctx context.Context, a APIClientInterface, projectId, serverId string) *wait.AsyncActionHandler[iaasalpha.Server] {
+	handler := wait.New(func() (waitFinished bool, response *iaasalpha.Server, err error) {
+		server, err := resizingServerWaitHandler(ctx, a, projectId, serverId).WaitWithContext(ctx)
+		if err != nil {
+			return false, server, err
+		}
+		server, err = a.GetServerExecute(ctx, projectId, serverId)
+		if err != nil {
+			return false, server, err
+		}
+		if server.Id == nil || server.Status == nil {
+			return false, server, fmt.Errorf("resizing failed for server with id %s, the response is not valid: the id or the status are missing", serverId)
+		}
+		if *server.Id == serverId && *server.Status == ServerActiveStatus {
+			return true, server, nil
+		}
+		if *server.Id == serverId && *server.Status == ErrorStatus {
+			return true, server, fmt.Errorf("resizing failed for server with id %s", serverId)
+		}
+		return false, server, nil
+	})
+	handler.SetTimeout(20 * time.Minute)
+	return handler
+}
+
+// DeleteServerWaitHandler will wait for volume deletion
+func DeleteServerWaitHandler(ctx context.Context, a APIClientInterface, projectId, serverId string) *wait.AsyncActionHandler[iaasalpha.Server] {
+	handler := wait.New(func() (waitFinished bool, response *iaasalpha.Server, err error) {
+		server, err := a.GetServerExecute(ctx, projectId, serverId)
+		if err == nil {
+			if server != nil {
+				if server.Id == nil || server.Status == nil {
+					return false, server, fmt.Errorf("delete failed for server with id %s, the response is not valid: the id or the status are missing", serverId)
+				}
+				if *server.Id == serverId && *server.Status == DeleteSuccess {
+					return true, server, nil
+				}
+			}
+			return false, nil, nil
+		}
+		oapiErr, ok := err.(*oapierror.GenericOpenAPIError) //nolint:errorlint //complaining that error.As should be used to catch wrapped errors, but this error should not be wrapped
+		if !ok {
+			return false, server, fmt.Errorf("could not convert error to oapierror.GenericOpenAPIError: %w", err)
+		}
+		if oapiErr.StatusCode != http.StatusNotFound {
+			return false, server, err
+		}
+		return true, nil, nil
+	})
+	handler.SetTimeout(20 * time.Minute)
 	return handler
 }
--- services/iaasalpha/wait/wait_test.go
+++ services/iaasalpha/wait/wait_test.go
@@ -13,8 +13,10 @@ import (
 
 type apiClientMocked struct {
 	getVolumeFails bool
+	getServerFails bool
 	isDeleted      bool
 	resourceState  string
+	returnResizing bool
 }
 
 func (a *apiClientMocked) GetVolumeExecute(_ context.Context, _, _ string) (*iaasalpha.Volume, error) {
@@ -36,6 +38,33 @@ func (a *apiClientMocked) GetVolumeExecute(_ context.Context, _, _ string) (*iaa
 	}, nil
 }
 
+func (a *apiClientMocked) GetServerExecute(_ context.Context, _, _ string) (*iaasalpha.Server, error) {
+	if a.returnResizing {
+		a.returnResizing = false
+		return &iaasalpha.Server{
+			Id:     utils.Ptr("sid"),
+			Status: utils.Ptr(ServerResizingStatus),
+		}, nil
+	}
+
+	if a.isDeleted {
+		return nil, &oapierror.GenericOpenAPIError{
+			StatusCode: 404,
+		}
+	}
+
+	if a.getServerFails {
+		return nil, &oapierror.GenericOpenAPIError{
+			StatusCode: 500,
+		}
+	}
+
+	return &iaasalpha.Server{
+		Id:     utils.Ptr("sid"),
+		Status: &a.resourceState,
+	}, nil
+}
+
 func TestCreateVolumeWaitHandler(t *testing.T) {
 	tests := []struct {
 		desc          string
@@ -47,7 +76,7 @@ func TestCreateVolumeWaitHandler(t *testing.T) {
 		{
 			desc:          "create_succeeded",
 			getFails:      false,
-			resourceState: AvailableStatus,
+			resourceState: VolumeAvailableStatus,
 			wantErr:       false,
 			wantResp:      true,
 		},
@@ -152,6 +181,279 @@ func TestDeleteVolumeWaitHandler(t *testing.T) {
 			handler := DeleteVolumeWaitHandler(context.Background(), apiClient, "pid", "vid")
 
 			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		})
+	}
+}
+
+func TestCreateServerWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc          string
+		getFails      bool
+		resourceState string
+		wantErr       bool
+		wantResp      bool
+	}{
+		{
+			desc:          "create_succeeded",
+			getFails:      false,
+			resourceState: ServerActiveStatus,
+			wantErr:       false,
+			wantResp:      true,
+		},
+		{
+			desc:          "error_status",
+			getFails:      false,
+			resourceState: ErrorStatus,
+			wantErr:       true,
+			wantResp:      true,
+		},
+		{
+			desc:          "get_fails",
+			getFails:      true,
+			resourceState: "",
+			wantErr:       true,
+			wantResp:      false,
+		},
+		{
+			desc:          "timeout",
+			getFails:      false,
+			resourceState: "ANOTHER Status",
+			wantErr:       true,
+			wantResp:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			apiClient := &apiClientMocked{
+				getServerFails: tt.getFails,
+				resourceState:  tt.resourceState,
+			}
+
+			var wantRes *iaasalpha.Server
+			if tt.wantResp {
+				wantRes = &iaasalpha.Server{
+					Id:     utils.Ptr("sid"),
+					Status: &tt.resourceState,
+				}
+			}
+
+			handler := CreateServerWaitHandler(context.Background(), apiClient, "pid", "sid")
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		})
+	}
+}
+
+func TestDeleteServerWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc          string
+		getFails      bool
+		isDeleted     bool
+		resourceState string
+		wantErr       bool
+		wantResp      bool
+	}{
+		{
+			desc:      "delete_succeeded",
+			getFails:  false,
+			isDeleted: true,
+			wantErr:   false,
+			wantResp:  false,
+		},
+		{
+			desc:          "get_fails",
+			getFails:      true,
+			resourceState: "",
+			wantErr:       true,
+			wantResp:      false,
+		},
+		{
+			desc:          "timeout",
+			getFails:      false,
+			resourceState: "ANOTHER Status",
+			wantErr:       true,
+			wantResp:      false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			apiClient := &apiClientMocked{
+				getServerFails: tt.getFails,
+				isDeleted:      tt.isDeleted,
+				resourceState:  tt.resourceState,
+			}
+
+			var wantRes *iaasalpha.Server
+			if tt.wantResp {
+				wantRes = &iaasalpha.Server{
+					Id:     utils.Ptr("sid"),
+					Status: &tt.resourceState,
+				}
+			}
+
+			handler := DeleteServerWaitHandler(context.Background(), apiClient, "pid", "sid")
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		})
+	}
+}
+
+func TestResizingServerWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc          string
+		getFails      bool
+		resourceState string
+		wantErr       bool
+		wantResp      bool
+	}{
+		{
+			desc:          "resizing",
+			getFails:      false,
+			resourceState: ServerResizingStatus,
+			wantErr:       false,
+			wantResp:      true,
+		},
+		{
+			desc:          "error_status",
+			getFails:      false,
+			resourceState: ErrorStatus,
+			wantErr:       true,
+			wantResp:      true,
+		},
+		{
+			desc:          "get_fails",
+			getFails:      true,
+			resourceState: "",
+			wantErr:       true,
+			wantResp:      false,
+		},
+		{
+			desc:          "timeout",
+			getFails:      false,
+			resourceState: "ANOTHER Status",
+			wantErr:       true,
+			wantResp:      true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			apiClient := &apiClientMocked{
+				getServerFails: tt.getFails,
+				resourceState:  tt.resourceState,
+			}
+
+			var wantRes *iaasalpha.Server
+			if tt.wantResp {
+				wantRes = &iaasalpha.Server{
+					Id:     utils.Ptr("sid"),
+					Status: &tt.resourceState,
+				}
+			}
+
+			handler := resizingServerWaitHandler(context.Background(), apiClient, "pid", "sid")
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		})
+	}
+}
+
+func TestResizeServerWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc               string
+		getFails           bool
+		returnResizing     bool
+		finalResourceState string
+		wantErr            bool
+		wantResp           bool
+	}{
+		{
+			desc:               "resize_succeeded",
+			getFails:           false,
+			returnResizing:     true,
+			finalResourceState: ServerActiveStatus,
+			wantErr:            false,
+			wantResp:           true,
+		},
+		{
+			desc:               "resizing_status_is_never_returned",
+			getFails:           false,
+			returnResizing:     false,
+			finalResourceState: ServerActiveStatus,
+			wantErr:            true,
+			wantResp:           true,
+		},
+		{
+			desc:               "error_status",
+			getFails:           false,
+			returnResizing:     true,
+			finalResourceState: ErrorStatus,
+			wantErr:            true,
+			wantResp:           true,
+		},
+		{
+			desc:               "get_fails",
+			getFails:           true,
+			finalResourceState: "",
+			wantErr:            true,
+			wantResp:           false,
+		},
+		{
+			desc:               "timeout",
+			getFails:           false,
+			finalResourceState: "ANOTHER Status",
+			wantErr:            true,
+			wantResp:           true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			apiClient := &apiClientMocked{
+				getServerFails: tt.getFails,
+				resourceState:  tt.finalResourceState,
+				returnResizing: tt.returnResizing,
+			}
+
+			var wantRes *iaasalpha.Server
+			if tt.wantResp {
+				wantRes = &iaasalpha.Server{
+					Id:     utils.Ptr("sid"),
+					Status: &tt.finalResourceState,
+				}
+			}
+
+			timeoutCtx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
+			defer cancel()
+			handler := ResizeServerWaitHandler(timeoutCtx, apiClient, "pid", "sid")
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(timeoutCtx)
 
 			if (err != nil) != tt.wantErr {
 				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
