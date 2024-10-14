4fe5a0f968a762d59047cf21e06e989d38dded6f
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2024-10-14 17:14:03 +0200

    Generator: Update SDK /services/sqlserverflex (#923)
    * Generate sqlserverflex
    
    * Update changelog
    
    ---------
    
    Co-authored-by: Vicente Pinto <vicente.pinto@freiheit.com>

--- services/sqlserverflex/model_instance_list_user.go
+++ services/sqlserverflex/model_instance_list_user.go
@@ -10,8 +10,143 @@ API version: 1.0.0
 
 package sqlserverflex
 
+import (
+	"encoding/json"
+)
+
+// checks if the InstanceListUser type satisfies the MappedNullable interface at compile time
+var _ MappedNullable = &InstanceListUser{}
+
 // InstanceListUser struct for InstanceListUser
 type InstanceListUser struct {
 	Id       *string `json:"id,omitempty"`
 	Username *string `json:"username,omitempty"`
+}
+
+// NewInstanceListUser instantiates a new InstanceListUser object
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed
+func NewInstanceListUser() *InstanceListUser {
+	this := InstanceListUser{}
+	return &this
+}
+
+// NewInstanceListUserWithDefaults instantiates a new InstanceListUser object
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set
+func NewInstanceListUserWithDefaults() *InstanceListUser {
+	this := InstanceListUser{}
+	return &this
+}
+
+// GetId returns the Id field value if set, zero value otherwise.
+func (o *InstanceListUser) GetId() *string {
+	if o == nil || IsNil(o.Id) {
+		var ret *string
+		return ret
+	}
+	return o.Id
+}
+
+// GetIdOk returns a tuple with the Id field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *InstanceListUser) GetIdOk() (*string, bool) {
+	if o == nil || IsNil(o.Id) {
+		return nil, false
+	}
+	return o.Id, true
+}
+
+// HasId returns a boolean if a field has been set.
+func (o *InstanceListUser) HasId() bool {
+	if o != nil && !IsNil(o.Id) {
+		return true
+	}
+
+	return false
+}
+
+// SetId gets a reference to the given string and assigns it to the Id field.
+func (o *InstanceListUser) SetId(v *string) {
+	o.Id = v
+}
+
+// GetUsername returns the Username field value if set, zero value otherwise.
+func (o *InstanceListUser) GetUsername() *string {
+	if o == nil || IsNil(o.Username) {
+		var ret *string
+		return ret
+	}
+	return o.Username
+}
+
+// GetUsernameOk returns a tuple with the Username field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *InstanceListUser) GetUsernameOk() (*string, bool) {
+	if o == nil || IsNil(o.Username) {
+		return nil, false
+	}
+	return o.Username, true
+}
+
+// HasUsername returns a boolean if a field has been set.
+func (o *InstanceListUser) HasUsername() bool {
+	if o != nil && !IsNil(o.Username) {
+		return true
+	}
+
+	return false
+}
+
+// SetUsername gets a reference to the given string and assigns it to the Username field.
+func (o *InstanceListUser) SetUsername(v *string) {
+	o.Username = v
+}
+
+func (o InstanceListUser) ToMap() (map[string]interface{}, error) {
+	toSerialize := map[string]interface{}{}
+	if !IsNil(o.Id) {
+		toSerialize["id"] = o.Id
+	}
+	if !IsNil(o.Username) {
+		toSerialize["username"] = o.Username
+	}
+	return toSerialize, nil
+}
+
+type NullableInstanceListUser struct {
+	value *InstanceListUser
+	isSet bool
+}
+
+func (v NullableInstanceListUser) Get() *InstanceListUser {
+	return v.value
+}
+
+func (v *NullableInstanceListUser) Set(val *InstanceListUser) {
+	v.value = val
+	v.isSet = true
+}
+
+func (v NullableInstanceListUser) IsSet() bool {
+	return v.isSet
+}
+
+func (v *NullableInstanceListUser) Unset() {
+	v.value = nil
+	v.isSet = false
+}
+
+func NewNullableInstanceListUser(val *InstanceListUser) *NullableInstanceListUser {
+	return &NullableInstanceListUser{value: val, isSet: true}
+}
+
+func (v NullableInstanceListUser) MarshalJSON() ([]byte, error) {
+	return json.Marshal(v.value)
+}
+
+func (v *NullableInstanceListUser) UnmarshalJSON(src []byte) error {
+	v.isSet = true
+	return json.Unmarshal(src, &v.value)
 }
--- services/sqlserverflex/model_list_backups_response.go
+++ services/sqlserverflex/model_list_backups_response.go
@@ -10,7 +10,107 @@ API version: 1.0.0
 
 package sqlserverflex
 
+import (
+	"encoding/json"
+)
+
+// checks if the ListBackupsResponse type satisfies the MappedNullable interface at compile time
+var _ MappedNullable = &ListBackupsResponse{}
+
 // ListBackupsResponse struct for ListBackupsResponse
 type ListBackupsResponse struct {
 	Databases *[]BackupListBackupsResponseGrouped `json:"databases,omitempty"`
+}
+
+// NewListBackupsResponse instantiates a new ListBackupsResponse object
+// This constructor will assign default values to properties that have it defined,
+// and makes sure properties required by API are set, but the set of arguments
+// will change when the set of required properties is changed
+func NewListBackupsResponse() *ListBackupsResponse {
+	this := ListBackupsResponse{}
+	return &this
+}
+
+// NewListBackupsResponseWithDefaults instantiates a new ListBackupsResponse object
+// This constructor will only assign default values to properties that have it defined,
+// but it doesn't guarantee that properties required by API are set
+func NewListBackupsResponseWithDefaults() *ListBackupsResponse {
+	this := ListBackupsResponse{}
+	return &this
+}
+
+// GetDatabases returns the Databases field value if set, zero value otherwise.
+func (o *ListBackupsResponse) GetDatabases() *[]BackupListBackupsResponseGrouped {
+	if o == nil || IsNil(o.Databases) {
+		var ret *[]BackupListBackupsResponseGrouped
+		return ret
+	}
+	return o.Databases
+}
+
+// GetDatabasesOk returns a tuple with the Databases field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *ListBackupsResponse) GetDatabasesOk() (*[]BackupListBackupsResponseGrouped, bool) {
+	if o == nil || IsNil(o.Databases) {
+		return nil, false
+	}
+	return o.Databases, true
+}
+
+// HasDatabases returns a boolean if a field has been set.
+func (o *ListBackupsResponse) HasDatabases() bool {
+	if o != nil && !IsNil(o.Databases) {
+		return true
+	}
+
+	return false
+}
+
+// SetDatabases gets a reference to the given []BackupListBackupsResponseGrouped and assigns it to the Databases field.
+func (o *ListBackupsResponse) SetDatabases(v *[]BackupListBackupsResponseGrouped) {
+	o.Databases = v
+}
+
+func (o ListBackupsResponse) ToMap() (map[string]interface{}, error) {
+	toSerialize := map[string]interface{}{}
+	if !IsNil(o.Databases) {
+		toSerialize["databases"] = o.Databases
+	}
+	return toSerialize, nil
+}
+
+type NullableListBackupsResponse struct {
+	value *ListBackupsResponse
+	isSet bool
+}
+
+func (v NullableListBackupsResponse) Get() *ListBackupsResponse {
+	return v.value
+}
+
+func (v *NullableListBackupsResponse) Set(val *ListBackupsResponse) {
+	v.value = val
+	v.isSet = true
+}
+
+func (v NullableListBackupsResponse) IsSet() bool {
+	return v.isSet
+}
+
+func (v *NullableListBackupsResponse) Unset() {
+	v.value = nil
+	v.isSet = false
+}
+
+func NewNullableListBackupsResponse(val *ListBackupsResponse) *NullableListBackupsResponse {
+	return &NullableListBackupsResponse{value: val, isSet: true}
+}
+
+func (v NullableListBackupsResponse) MarshalJSON() ([]byte, error) {
+	return json.Marshal(v.value)
+}
+
+func (v *NullableListBackupsResponse) UnmarshalJSON(src []byte) error {
+	v.isSet = true
+	return json.Unmarshal(src, &v.value)
 }
