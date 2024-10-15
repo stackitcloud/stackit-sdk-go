43716f7df7cbe7064441611b023a91251bc078de
GokceGK <161626272+GokceGK@users.noreply.github.com>
2024-10-15 10:11:42 +0200

    fix typo (#928)
    Co-authored-by: Gökçe Gök Klingel <goekce.goek_klingel@stackit.cloud>

--- examples/iaasalpha/attach_security_group/attach_security_group.go
+++ examples/iaasalpha/attach_security_group/attach_security_group.go
@@ -63,5 +63,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("[iaasalpha API] SecurityGroup %q has been successfully detacched from the server %s.\n", securityGroupId, serverId)
+	fmt.Printf("[iaasalpha API] SecurityGroup %q has been successfully detached from the server %s.\n", securityGroupId, serverId)
 }
