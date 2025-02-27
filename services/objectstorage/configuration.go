f61f680d9ca19b81f5bfaaef4fa68becf0bdd0b7
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2025-02-27 15:35:34 +0100

    Generate objectstorage (#1509)

--- services/objectstorage/configuration.go
+++ services/objectstorage/configuration.go
@@ -22,16 +22,12 @@ func NewConfiguration() *config.Configuration {
 		Debug:         false,
 		Servers: config.ServerConfigurations{
 			{
-				URL:         "https://object-storage.api.{region}stackit.cloud",
+				URL:         "https://object-storage.api.stackit.cloud",
 				Description: "No description provided",
 				Variables: map[string]config.ServerVariable{
 					"region": {
 						Description:  "No description provided",
-						DefaultValue: "eu01.",
-						EnumValues: []string{
-							"eu01.",
-							"eu02.",
-						},
+						DefaultValue: "global",
 					},
 				},
 			},
