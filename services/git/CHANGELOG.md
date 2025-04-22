1cca1833bfb01d3bb19d8aea93ec5afc44ef0b98
Pere Manent <pere.manent@mail.schwarz>
2025-04-22 16:55:58 +0200

    Improve waiters test and adding changelog to the git service

--- services/git/CHANGELOG.md
+++ services/git/CHANGELOG.md
@@ -1,3 +1,6 @@
+## v0.3.0 (2025-04-22)
+- **Features**: Add waiters to manage the STACKIT Git
+
 ## v0.2.0 (2025-04-16)
 - **Features**: Add new methods to manage the STACKIT Git: `CreateInstance`, `DeleteInstance`, `GetInstance`
 
--- services/git/wait/wait.go
+++ services/git/wait/wait.go
@@ -2,9 +2,12 @@ package wait
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/stackitcloud/stackit-sdk-go/core/oapierror"
 	"github.com/stackitcloud/stackit-sdk-go/core/wait"
 	"github.com/stackitcloud/stackit-sdk-go/services/git"
 )
@@ -43,14 +46,18 @@ func CreateGitInstanceWaitHandler(ctx context.Context, a APIClientInterface, pro
 
 func DeleteGitInstanceWaitHandler(ctx context.Context, a APIClientInterface, projectId, instanceId string) *wait.AsyncActionHandler[git.Instance] {
 	handler := wait.New(func() (waitFinished bool, response *git.Instance, err error) {
-		instance, err := a.GetInstanceExecute(ctx, projectId, instanceId)
-		if err != nil {
-			return false, nil, err
+		_, err = a.GetInstanceExecute(ctx, projectId, instanceId)
+		// the instances is still gettable, e.g. not deleted, when the errors is null
+		if err == nil {
+			return false, nil, nil
 		}
-		if instance != nil {
-			return false, instance, nil
+		var oapiError *oapierror.GenericOpenAPIError
+		if errors.As(err, &oapiError) {
+			if statusCode := oapiError.StatusCode; statusCode == http.StatusNotFound {
+				return true, nil, nil
+			}
 		}
-		return true, nil, nil
+		return false, nil, err
 	})
 	handler.SetTimeout(10 * time.Minute)
 	return handler
