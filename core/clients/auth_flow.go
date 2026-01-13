a987bfa58d450007ff168b1934a1adff50bee2aa
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2026-01-13 14:02:38 +0100

    Update core/clients/auth_flow.go
    Co-authored-by: Marcel Jacek <72880145+marceljk@users.noreply.github.com>

--- core/clients/auth_flow.go
+++ core/clients/auth_flow.go
@@ -27,6 +27,8 @@ type AuthFlow interface {
 type TokenResponseBody struct {
 	AccessToken string `json:"access_token"`
 	ExpiresIn   int    `json:"expires_in"`
+	// Deprecated
+	RefreshToken string `json:"refresh_token"`
 	Scope       string `json:"scope"`
 	TokenType   string `json:"token_type"`
 }
