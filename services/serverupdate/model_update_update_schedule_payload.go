47e0f290eeb62deccd9502aa572bbf7173974705
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2026-02-10 11:35:10 +0100

    Generate serverupdate (#4805)

--- services/serverupdate/model_update_update_schedule_payload.go
+++ services/serverupdate/model_update_update_schedule_payload.go
@@ -103,11 +103,13 @@ type UpdateUpdateSchedulePayloadGetRruleRetType = string
 type UpdateUpdateSchedulePayload struct {
 	// REQUIRED
 	Enabled UpdateUpdateSchedulePayloadgetEnabledAttributeType `json:"enabled" required:"true"`
+	// Updates start within the defined hourly window. Depending on the updates, the process may exceed this timeframe and require an automatic restart.
 	// Can be cast to int32 without loss of precision.
 	// REQUIRED
 	MaintenanceWindow UpdateUpdateSchedulePayloadGetMaintenanceWindowAttributeType `json:"maintenanceWindow" required:"true"`
 	// REQUIRED
 	Name UpdateUpdateSchedulePayloadGetNameAttributeType `json:"name" required:"true"`
+	// An rrule (Recurrence Rule) is a standardized string format used in iCalendar (RFC 5545) to define repeating events, and you can generate one by using a dedicated library or by using online generator tools to specify parameters like frequency, interval, and end dates
 	// REQUIRED
 	Rrule UpdateUpdateSchedulePayloadGetRruleAttributeType `json:"rrule" required:"true"`
 }
