ce02b9c48bf2d1d83e7721d4953caaeedae84e92
Ruben Hoenle <Ruben.Hoenle@stackit.cloud>
2026-03-09 12:06:16 +0100

    examples, waiters, changelogs

--- examples/mariadb/mariadb.go
+++ examples/mariadb/mariadb.go
@@ -6,8 +6,7 @@ import (
 	"os"
 
 	"github.com/stackitcloud/stackit-sdk-go/core/config"
-	"github.com/stackitcloud/stackit-sdk-go/core/utils"
-	"github.com/stackitcloud/stackit-sdk-go/services/mariadb"
+	mariadb "github.com/stackitcloud/stackit-sdk-go/services/mariadb/v1api"
 )
 
 func main() {
@@ -24,15 +23,15 @@ func main() {
 	}
 
 	// Get the mariadb instances for your project
-	getInstancesResp, err := mariadbClient.ListInstances(context.Background(), projectId).Execute()
+	getInstancesResp, err := mariadbClient.DefaultAPI.ListInstances(context.Background(), projectId).Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `GetInstances`: %v\n", err)
 	} else {
-		fmt.Printf("Number of instances: %v\n", len(*getInstancesResp.Instances))
+		fmt.Printf("Number of instances: %v\n", len(getInstancesResp.Instances))
 	}
 
 	// Get the mariadb offerings for your project
-	getOfferingsResp, err := mariadbClient.ListOfferings(context.Background(), projectId).Execute()
+	getOfferingsResp, err := mariadbClient.DefaultAPI.ListOfferings(context.Background(), projectId).Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `GetOfferings`: %v\n", err)
 	} else {
@@ -41,14 +40,14 @@ func main() {
 
 	// Create a mariadb Instance
 	createInstancePayload := mariadb.CreateInstancePayload{
-		InstanceName: utils.Ptr("exampleInstance"),
+		InstanceName: "exampleInstance",
 		Parameters:   &mariadb.InstanceParameters{},
-		PlanId:       utils.Ptr(planId),
+		PlanId:       planId,
 	}
-	createInstanceResp, err := mariadbClient.CreateInstance(context.Background(), projectId).CreateInstancePayload(createInstancePayload).Execute()
+	createInstanceResp, err := mariadbClient.DefaultAPI.CreateInstance(context.Background(), projectId).CreateInstancePayload(createInstancePayload).Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `CreateInstance`: %v\n", err)
 	} else {
-		fmt.Printf("Created instance with instance id \"%s\".\n", *createInstanceResp.InstanceId)
+		fmt.Printf("Created instance with instance id \"%s\".\n", createInstanceResp.InstanceId)
 	}
 }
