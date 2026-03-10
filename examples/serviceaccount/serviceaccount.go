f619dad2c1932c12db129d72023468eb05b7fb94
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2026-03-10 13:21:44 +0100

    feat(serviceaccount): add multi API version support (#5778)
    relates to STACKITSDK-357
    
    Co-authored-by: Ruben Hoenle <Ruben.Hoenle@stackit.cloud>

--- examples/serviceaccount/serviceaccount.go
+++ examples/serviceaccount/serviceaccount.go
@@ -5,8 +5,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/stackitcloud/stackit-sdk-go/core/utils"
-	"github.com/stackitcloud/stackit-sdk-go/services/serviceaccount"
+	serviceaccount "github.com/stackitcloud/stackit-sdk-go/services/serviceaccount/v2api"
 )
 
 func main() {
@@ -20,21 +19,21 @@ func main() {
 	}
 
 	// Get the service accounts for your project
-	getAccountsResp, err := client.ListServiceAccounts(context.Background(), projectId).Execute()
+	getAccountsResp, err := client.DefaultAPI.ListServiceAccounts(context.Background(), projectId).Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `GetServiceAccounts`: %v\n", err)
 	} else {
-		fmt.Printf("Number of service accounts: %v\n", len(*getAccountsResp.Items))
+		fmt.Printf("Number of service accounts: %v\n", len(getAccountsResp.Items))
 	}
 
 	// Create a service account
 	createAccountPayload := serviceaccount.CreateServiceAccountPayload{
-		Name: utils.Ptr("my-service-account"),
+		Name: "my-service-account",
 	}
-	createAccountResp, err := client.CreateServiceAccount(context.Background(), projectId).CreateServiceAccountPayload(createAccountPayload).Execute()
+	createAccountResp, err := client.DefaultAPI.CreateServiceAccount(context.Background(), projectId).CreateServiceAccountPayload(createAccountPayload).Execute()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `CreateServiceAccount`: %v\n", err)
 	} else {
-		fmt.Printf("Created service account with email \"%s\".\n", *createAccountResp.Email)
+		fmt.Printf("Created service account with email \"%s\".\n", createAccountResp.Email)
 	}
 }
