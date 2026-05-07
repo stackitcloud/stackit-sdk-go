bab63653068c6f402ca8558256c01674f7011ae3
Bjarne Schröder <42645588+bjarneschroeder@users.noreply.github.com>
2026-05-07 13:55:44 +0200

    Dremio: Apply suggestions from code review for Dremio SDK example.
    Co-authored-by: cgoetz-inovex <carlo.goetz@inovex.de>

--- examples/dremio/dremio.go
+++ examples/dremio/dremio.go
@@ -7,15 +7,21 @@ import (
 
 	"github.com/stackitcloud/stackit-sdk-go/core/utils"
 	dremio "github.com/stackitcloud/stackit-sdk-go/services/dremio/v1alphaapi"
+    "github.com/stackitcloud/stackit-sdk-go/services/dremio/v1alphaapi/wait/wait"
 )
 
 func main() {
 	region := "eu01"          // Region where the resources will be created
 	projectId := "PROJECT_ID" // Your STACKIT project ID
+	serviceAccountKeyPath := "sa-key-path.json" // Path to your STACKIT service account json
+
 
 	ctx := context.Background()
 
-	dremioClient, err := dremio.NewAPIClient()
+	dremioClient, err := dremio.NewAPIClient(
+		config.WithRegion(region),
+		config.WithServiceAccountKeyPath(serviceAccountKeyPath),
+	)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "[Dremio] Creating API client: %v\n", err)
 		os.Exit(1)
@@ -34,6 +40,39 @@ func main() {
 	dremioId := createDremioInstanceResponse.Id
 	fmt.Printf("[Dremio] Triggered creation of Dremio with ID: %s. Waiting for it to become active... \n", dremioId)
 
+	_, err = wait.CreateDremioWaitHandler(ctx, dremioClient.DefaultAPI, projectId, region, dremioId).WaitWithContext(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when waiting for creation: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Update a Dremio instance
+	_, err = dremioClient.DefaultAPI.UpdateDremioInstance(ctx, projectId, region, dremioId).UpdateDremioInstancePayload(dremio.UpdateDremioInstancePayload{
+		DisplayName: utils.Ptr("myExampleDremioInstanceUpdated"),
+	}).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when updating Dremio instance: %v\n", err)
+		os.Exit(1)
+	}
+	_, err = wait.UpdateDremioWaitHandler(ctx, dremioClient.DefaultAPI, projectId, region, dremioId).WaitWithContext(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when waiting for update: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Listing Dremio instances
+	listResp, err := dremioClient.DefaultAPI.ListDremioInstances(ctx, projectId, region).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when listing Dremio instances: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Dremio instances:")
+	for _, instance := range listResp.Dremios {
+		fmt.Printf("%s - %s\n", instance.Id, instance.DisplayName)
+	}
+	fmt.Println()
+
+
 	// Creating a Dremio user
 	createDremioUserPayload := dremio.CreateDremioUserPayload{
 		Description: utils.Ptr("This is a new user."),
@@ -50,4 +89,48 @@ func main() {
 	}
 	dremioUserId := createDremioUserResponse.Id
 	fmt.Printf("[Dremio] Created Dremio User with ID: %s\n", dremioUserId)
+
+	_, err = wait.CreateDremioUserWaitHandler(ctx, dremioClient.DefaultAPI, projectId, region, dremioId, dremioUserId).WaitWithContext(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when waiting for user creation: %v\n", err)
+		os.Exit(1)
+	}
+
+	// List Dremio users
+	listUsersResp, err := dremioClient.DefaultAPI.ListDremioUsers(ctx, projectId, region, dremioId).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when listing Dremio users: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Dremio users:")
+	for _, user := range listUsersResp.DremioUsers {
+		fmt.Printf("%s - %s\n", user.Id, user.Name)
+	}
+	fmt.Println()
+
+	// Delete a Dremio user
+	err = dremioClient.DefaultAPI.DeleteDremioUser(ctx, projectId, region, dremioId, dremioUserId).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when deleting Dremio user: %v\n", err)
+		os.Exit(1)
+	}
+	_, err = wait.DeleteDremioUserWaitHandler(ctx, dremioClient.DefaultAPI, projectId, region, dremioId, dremioUserId).WaitWithContext(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when waiting for user deletion: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Delete a Dremio instance
+	err = dremioClient.DefaultAPI.DeleteDremioInstance(ctx, projectId, region, dremioId).Execute()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when deleting Dremio instance: %v\n", err)
+		os.Exit(1)
+	}
+	_, err = wait.DeleteDremioWaitHandler(ctx, dremioClient.DefaultAPI, projectId, region, dremioId).WaitWithContext(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "[Dremio] Error when waiting for deletion: %v\n", err)
+		os.Exit(1)
+	}
+
+
 }
