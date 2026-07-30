0c3ffcb4e751c6b1f3c52d0faf85062931dd19df
Alexander Dahmen <alexander.dahmen@inovex.de>
2026-07-30 14:02:59 +0200

    feat(mariadb): Add wait handler v2 (#9350)
    STACKITSDK-462
    
    Signed-off-by: Alexander Dahmen <alexander.dahmen@inovex.de>

---
+++ services/mariadb/v2api/wait/wait_test.go
@@ -0,0 +1,331 @@
+package wait
+
+import (
+	"context"
+	"fmt"
+	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+
+	"github.com/stackitcloud/stackit-sdk-go/core/oapierror"
+	"github.com/stackitcloud/stackit-sdk-go/core/utils"
+	"github.com/stackitcloud/stackit-sdk-go/core/wait"
+	mariadb "github.com/stackitcloud/stackit-sdk-go/services/mariadb/v2api"
+)
+
+const deleteOperation = "delete"
+
+const testRegion = "eu01"
+
+type mockSettings struct {
+	instanceGetFails                   bool
+	instanceDeletionSucceedsWithErrors bool
+	instanceResourceId                 string
+	instanceResourceOperation          *string
+	instanceResourceState              *mariadb.InstanceStatus
+	instanceResourceDescription        string
+
+	credentialGetFails          bool
+	credentialResourceId        string
+	credentialOperationSucceeds bool
+	credentialDeletionSucceeds  bool
+}
+
+func newAPIMock(settings *mockSettings) mariadb.DefaultAPI {
+	return &mariadb.DefaultAPIServiceMock{
+		GetInstanceExecuteMock: utils.Ptr(func(_ mariadb.ApiGetInstanceRequest) (*mariadb.Instance, error) {
+			if settings.instanceGetFails {
+				return nil, &oapierror.GenericOpenAPIError{
+					StatusCode: 500,
+				}
+			}
+			if settings.instanceResourceOperation != nil && *settings.instanceResourceOperation == deleteOperation && settings.instanceResourceState != nil && *settings.instanceResourceState == mariadb.INSTANCESTATUS_ACTIVE {
+				if settings.instanceDeletionSucceedsWithErrors {
+					return &mariadb.Instance{
+						InstanceId: &settings.instanceResourceId,
+						Status:     settings.instanceResourceState,
+						LastOperation: mariadb.InstanceLastOperation{
+							Description: settings.instanceResourceDescription,
+						},
+					}, nil
+				}
+				return nil, &oapierror.GenericOpenAPIError{
+					StatusCode: 410,
+				}
+			}
+
+			return &mariadb.Instance{
+				InstanceId: &settings.instanceResourceId,
+				Status:     settings.instanceResourceState,
+			}, nil
+		}),
+		GetCredentialsExecuteMock: utils.Ptr(func(_ mariadb.ApiGetCredentialsRequest) (*mariadb.CredentialsResponse, error) {
+			if settings.credentialGetFails {
+				return nil, &oapierror.GenericOpenAPIError{
+					StatusCode: 500,
+				}
+			}
+
+			if !settings.credentialOperationSucceeds || settings.credentialDeletionSucceeds {
+				return nil, &oapierror.GenericOpenAPIError{
+					StatusCode: 404,
+				}
+			}
+
+			return &mariadb.CredentialsResponse{
+				Id: settings.credentialResourceId,
+			}, nil
+		}),
+	}
+}
+
+func TestCreateOrUpdateInstanceWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc          string
+		getFails      bool
+		resourceState *mariadb.InstanceStatus
+		wantErr       bool
+		wantResp      bool
+	}{
+		{
+			desc:          "succeeded",
+			getFails:      false,
+			resourceState: utils.Ptr(mariadb.INSTANCESTATUS_ACTIVE),
+			wantErr:       false,
+			wantResp:      true,
+		},
+		{
+			desc:          "failed",
+			getFails:      false,
+			resourceState: utils.Ptr(mariadb.INSTANCESTATUS_FAILED),
+			wantErr:       true,
+			wantResp:      true,
+		},
+		{
+			desc:     "get_fails",
+			getFails: true,
+			wantErr:  true,
+			wantResp: false,
+		},
+		{
+			desc:          "timeout",
+			getFails:      false,
+			resourceState: utils.Ptr(mariadb.INSTANCESTATUS_UNKNOWN_DEFAULT_OPEN_API),
+			wantErr:       true,
+			wantResp:      false,
+		},
+	}
+
+	handlers := map[string]func(context.Context, mariadb.DefaultAPI, string, string, string) *wait.AsyncActionHandler[mariadb.Instance]{
+		"common logic": createOrUpdateInstanceWaitHandler,
+		"create":       CreateInstanceWaitHandler,
+		"update":       PartialUpdateInstanceWaitHandler,
+	}
+
+	for handlerDesc, handlerFn := range handlers {
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%s - %s", handlerDesc, tt.desc), func(t *testing.T) {
+				synctest.Test(t, func(t *testing.T) {
+					instanceId := "foo-bar"
+
+					apiClient := newAPIMock(&mockSettings{
+						instanceGetFails:      tt.getFails,
+						instanceResourceId:    instanceId,
+						instanceResourceState: tt.resourceState,
+					})
+
+					var wantRes *mariadb.Instance
+					if tt.wantResp {
+						wantRes = &mariadb.Instance{
+							InstanceId: &instanceId,
+							Status:     tt.resourceState,
+						}
+					}
+
+					handler := handlerFn(context.Background(), apiClient, "pid", testRegion, instanceId)
+					gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+
+					if (err != nil) != tt.wantErr {
+						t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+					}
+					diff := cmp.Diff(gotRes, wantRes)
+					if diff != "" {
+						t.Fatalf("handler gotRes = %+v\n want %+v\n diff = %s", gotRes, wantRes, diff)
+					}
+				})
+			})
+		}
+	}
+}
+
+func TestDeleteInstanceWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc                      string
+		getFails                  bool
+		deleteSucceeedsWithErrors bool
+		resourceState             *mariadb.InstanceStatus
+		resourceDescription       string
+		wantErr                   bool
+	}{
+		{
+			desc:                      "delete_succeeded",
+			getFails:                  false,
+			deleteSucceeedsWithErrors: false,
+			resourceState:             utils.Ptr(mariadb.INSTANCESTATUS_ACTIVE),
+			wantErr:                   false,
+		},
+		{
+			desc:                      "delete_failed",
+			getFails:                  false,
+			deleteSucceeedsWithErrors: false,
+			resourceState:             utils.Ptr(mariadb.INSTANCESTATUS_FAILED),
+			wantErr:                   true,
+		},
+		{
+			desc:                      "delete_succeeds_with_errors",
+			getFails:                  false,
+			resourceState:             utils.Ptr(mariadb.INSTANCESTATUS_ACTIVE),
+			deleteSucceeedsWithErrors: true,
+			resourceDescription:       "Deleting resource: cf failed with error: DeleteFailed",
+			wantErr:                   true,
+		},
+		{
+			desc:                      "get_fails",
+			deleteSucceeedsWithErrors: false,
+			getFails:                  true,
+			wantErr:                   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			instanceId := "foo-bar"
+
+			apiClient := newAPIMock(&mockSettings{
+				instanceGetFails:                   tt.getFails,
+				instanceDeletionSucceedsWithErrors: tt.deleteSucceeedsWithErrors,
+				instanceResourceId:                 instanceId,
+				instanceResourceOperation:          utils.Ptr(deleteOperation),
+				instanceResourceDescription:        tt.resourceDescription,
+				instanceResourceState:              tt.resourceState,
+			})
+
+			handler := DeleteInstanceWaitHandler(context.Background(), apiClient, "", testRegion, instanceId)
+
+			_, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestCreateCredentialsWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc              string
+		getFails          bool
+		operationSucceeds bool
+		wantErr           bool
+		wantResp          bool
+	}{
+		{
+			desc:              "create_succeeded",
+			getFails:          false,
+			operationSucceeds: true,
+			wantErr:           false,
+			wantResp:          true,
+		},
+		{
+			desc:              "create_failed",
+			getFails:          false,
+			operationSucceeds: false,
+			wantErr:           true,
+			wantResp:          false,
+		},
+		{
+			desc:     "get_fails",
+			getFails: true,
+			wantErr:  true,
+			wantResp: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			credentialsId := "foo-bar"
+
+			apiClient := newAPIMock(&mockSettings{
+				credentialGetFails:          tt.getFails,
+				credentialResourceId:        credentialsId,
+				credentialOperationSucceeds: tt.operationSucceeds,
+			})
+
+			var wantRes *mariadb.CredentialsResponse
+			if tt.wantResp {
+				wantRes = &mariadb.CredentialsResponse{
+					Id: credentialsId,
+				}
+			}
+
+			handler := CreateCredentialsWaitHandler(context.Background(), apiClient, "", testRegion, "", credentialsId)
+
+			gotRes, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !cmp.Equal(gotRes, wantRes) {
+				t.Fatalf("handler gotRes = %v, want %v", gotRes, wantRes)
+			}
+		})
+	}
+}
+
+func TestDeleteCredentialsWaitHandler(t *testing.T) {
+	tests := []struct {
+		desc             string
+		getFails         bool
+		deletionSucceeds bool
+		wantErr          bool
+	}{
+		{
+			desc:             "delete_succeeded",
+			getFails:         false,
+			deletionSucceeds: true,
+			wantErr:          false,
+		},
+		{
+			desc:             "delete_failed",
+			getFails:         false,
+			deletionSucceeds: false,
+			wantErr:          true,
+		},
+		{
+			desc:             "get_fails",
+			getFails:         true,
+			deletionSucceeds: false,
+			wantErr:          true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			credentialsId := "foo-bar"
+
+			apiClient := newAPIMock(&mockSettings{
+				credentialGetFails:          tt.getFails,
+				credentialResourceId:        credentialsId,
+				credentialOperationSucceeds: true,
+				credentialDeletionSucceeds:  tt.deletionSucceeds,
+			})
+
+			handler := DeleteCredentialsWaitHandler(context.Background(), apiClient, "", testRegion, "", credentialsId)
+
+			_, err := handler.SetTimeout(10 * time.Millisecond).WaitWithContext(context.Background())
+
+			if (err != nil) != tt.wantErr {
+				t.Fatalf("handler error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
